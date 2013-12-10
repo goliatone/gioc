626641486160d35ec36862092072fca6d2f2d90f
goliatone <burgosemi@gmail.com>
2013-12-09 21:33:11 -0500

    Spec: WIP.

--- test/spec/gioc-spec.js
+++ test/spec/gioc-spec.js
@@ -16,18 +16,15 @@ define(['gioc', 'jquery'], function(Gioc, $) {
 
         it('should contain known methods.', function() {
             var gioc = new Gioc();
-            var methods = ['map', 'solve', 'configure', 'build', 
+            var methods = ['map', 'solve', 'prepare', 'configure', 'build', 
                            'wire', 'inject', 'post', 'mapped', 
                            'solveDependencies', 'error', 'log',
                            'addSolver', 'addPost', 'addProvider', 
                            'resetGraph', 'extend'
                            ];
-            var method;
-            for(var m in methods){
-                method = methods[m];
-                expect((method in gioc)).toBeTruthy();
+            methods.map(function(method){
                 expect((typeof gioc[method] === 'function')).toBeTruthy();
-            }
+            });
         });
 
         it('should add factories',function(){
@@ -56,7 +53,60 @@ define(['gioc', 'jquery'], function(Gioc, $) {
             });
             expect(gioc.solve('factory')).toBe(23);
         });
+    });
 
-        
+    describe('Gioc configuration', function(){
+        var gioc;
+
+        beforeEach(function(){
+            gioc = new Gioc();
+        });
+
+        it('should have a *static* config object', function(){
+            expect(Gioc.config).toBeTruthy();
+        });
+
+        it('config object should have an defaults prop', function(){
+            expect(Gioc.config.defaults).toBeTruthy();
+        });
+
+        it('config object should have an attributes prop', function(){
+            expect(Gioc.config.attributes).toBeTruthy();
+        });
+
+        it('defaults and attributes should be equal length', function(){
+            expect(Gioc.config.attributes).toMatchLengthOf(Gioc.config.defaults);
+        });
+
+        it('should create properties in config.attributes',function(){
+            Gioc.config.attributes.map(function(key){
+                expect(gioc[key]).toBeTruthy();
+            });
+        });
+
+        it('should set default values for properties in config.attributes',function(){
+            Gioc.config.attributes.map(function(key){
+                expect(gioc[key]).toBe(Gioc.config.defaults[key]);
+            });
+        });
+
+        it('should change static default values',function(){
+            var attrs    = Gioc.config.attributes,
+                defaults = Gioc.config.defaults,
+                value    = null;
+
+            attrs.map(function(attr){
+                value = defaults[attr];
+                defaults[attr] = value + 'MOD';
+            });
+
+            gioc = new Gioc();
+            // gioc.configure();
+
+            attrs.map(function(key){
+                expect(gioc[key]).toBe(defaults[key]);
+            });
+        });
+
     });
 });
