37ed689c8699762432da7f8751859bcbae06976f
goliatone <burgosemi@gmail.com>
2014-04-30 23:26:07 -0400

    Gruntfile: Refactored build process.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,8 +13,8 @@ module.exports = function (grunt) {
         name: 'gioc',
         src: 'src',
         dist: 'dist',
-        lib:'libs',
-        app:'app'
+        lib:'lib',
+        example:'example'
     };
 
     try {
@@ -29,17 +29,16 @@ module.exports = function (grunt) {
         watch: {
             livereload: {
                 files: [
-                    '<%= config.src %>/{,*/}*.html',
-                    '{.tmp,<%= config.src %>}/{,*/}*.css',
-                    '{.tmp,<%= config.src %>,<%= config.app %>}/{,*/}*.js',
-                    '<%= config.src %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                    '<%= config.example %>/*.html',
+                    '<%= config.example %>/*.js',
+                    '{.tmp,<%= config.src %>}/*.js'
                 ],
                 tasks: ['livereload']
             }
         },
         connect: {
             options: {
-                port: 9130,
+                port: 9100,
                 // Change this to '0.0.0.0' to access the server from outside.
                 hostname: 'localhost'
             },
@@ -50,15 +49,15 @@ module.exports = function (grunt) {
                             lrSnippet,
                             mountFolder(connect, '.tmp'),
                             mountFolder(connect, config.src),
-                            mountFolder(connect, config.app),
-                            mountFolder(connect, config.lib),
+                            mountFolder(connect, config.libs),
+                            mountFolder(connect, config.example)
                         ];
                     }
                 }
             },
             test: {
                 options: {
-                    port:9564,
+                    port:4545,
                     middleware: function (connect) {
                         return [
                             mountFolder(connect, '.tmp'),
@@ -101,15 +100,15 @@ module.exports = function (grunt) {
         karma: {
             options: {
                 configFile: 'karma.conf.js',
-                runnerPort: 9999,
-                browsers: ['Chrome', 'Firefox']
+                runnerPort: 9999
+                // browsers: ['Chrome', 'Firefox']
             },
             unit: {
                 reporters: 'dots'
             },
-            continuous: {
-                singleRun: true,
-                browsers: ['PhantomJS']
+            debug:{
+                singleRun:false,
+                browsers:['Chrome']
             },
             ci: {
                 singleRun: true,
@@ -144,6 +143,21 @@ module.exports = function (grunt) {
                     dest: '<%= config.dist %>',
                     src: []
                 }]
+            }
+        },
+        bump: {
+            options: {
+                files: ['package.json', 'bower.json', 'component.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json', 'bower.json', 'component.json'], // '-a' for all files
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
             }
         }
     });
